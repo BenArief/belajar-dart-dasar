3479b06ebb55e1e8c7104c7fbee29c9c6ca18614
BenArief <benarief29@gmail.com>
2026-03-25 22:22:34 +0700

    String Type data

---
+++ bin/dynamic.dart
@@ -0,0 +1,10 @@
+void main(){
+  dynamic variable = 100;
+  print("Init int:  $variable");
+
+  variable = true;
+  print("Ubah ke boolean:  $variable");
+
+  variable = 'string';
+  print("Ubah jadi string: $variable");
+}
