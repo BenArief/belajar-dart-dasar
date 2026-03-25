0fdcd68ee2ba2733ef7285a88d9de1871d8457e4
BenArief <benarief29@gmail.com>
2026-03-25 20:16:16 +0700

    Comments

---
+++ bin/comment.dart
@@ -0,0 +1,12 @@
+/// this is main function aka comments
+/// Will be Executed by Dart
+void main(){
+  // This is name variable
+  var name = "Nama";
+
+  print(name);
+  /*
+  This is multi line comments,
+  you can add more
+   */
+}
