ca816c527f26022c0f0a5fc6b03a951306a2cda0
BenArief <benarief29@gmail.com>
2026-03-25 20:12:51 +0700

    Variable in dart

--- bin/variable.dart
+++ bin/variable.dart
@@ -1,5 +1,29 @@
 void main(){
-  String name;
-  name = "Ben Arief";
+  var name = "Ben Arief";
+  final nama = "Windah";
+  // nama = "basudara";
+
   print(name);
+
+  name = "Ilham";
+  print(name);
+  print("Variabel yang tidak bisa diubah hasilnya : " + nama);
+
+  final array1 = [1,2,3]; // datanya gabisa diubah tapi isinya bisa
+  const array2 = [1,2,3]; // gabisa diubah sama sekali alias paten
+
+  array1[0] = 10;
+  // array2[0] = 10;
+
+  print(array1);
+  print(array2);
+
+  late var value = getValue();
+  print("Variabel sudah dibuat");
+  print(value);
+}
+
+String getValue(){
+  print("GetValue() dipanggil");
+  return "Ben Arief";
 }
